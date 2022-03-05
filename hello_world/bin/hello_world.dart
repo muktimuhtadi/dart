dcb78f7ed9dc2115d4db495232c20220f98fd3d0
Mukti Muhtadi <muktimuhtadi8@gmail.com>
2022-03-05 15:54:53 +0700

    komentar

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,7 +1,36 @@
+/// ini adalah dokumentasi pembelajaran dart oleh Mukti
+/// will executed by dart
+
 void main() {
   // print('Hello World!');
 
   String name = 'Mukti Muhtadi';
 
-  print(name);
+  // print(name);
+
+  var firstName = 'Mukti';
+  final lastName = 'Muhtadi';
+
+  /*
+  print(firstName);
+  print(lastName);
+  */
+
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  // array1[0] = 4;
+  // array2[0] = 4;
+
+  print(array1);
+  print(array2);
+
+  late var value = getValue();
+  print('Variable sudah dibuat');
+  print(value);
+}
+
+String getValue() {
+  print('getValue() dipanggil');
+  return 'Mukti Muhtadi';
 }
