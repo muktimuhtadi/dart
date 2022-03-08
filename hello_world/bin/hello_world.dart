fa21ed476ad25131874c530f0cca88c4781884a3
Mukti Muhtadi <muktimuhtadi8@gmail.com>
2022-03-08 21:45:31 +0700

    operator perbandingan

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -2,35 +2,173 @@
 /// will executed by dart
 
 void main() {
-  // print('Hello World!');
+//   print('Hello World!');
 
-  String name = 'Mukti Muhtadi';
+//   String name = 'Mukti Muhtadi';
 
-  // print(name);
+//   print(name);
 
-  var firstName = 'Mukti';
-  final lastName = 'Muhtadi';
+//   var firstName = 'Mukti';
+//   final lastName = 'Muhtadi';
 
-  /*
-  print(firstName);
-  print(lastName);
-  */
+//   /*
+//   print(firstName);
+//   print(lastName);
+//   */
 
-  final array1 = [1, 2, 3];
-  const array2 = [1, 2, 3];
+//   final array1 = [1, 2, 3];
+//   const array2 = [1, 2, 3];
 
-  // array1[0] = 4;
-  // array2[0] = 4;
+//   array1[0] = 4;
+//   array2[0] = 4;
 
-  print(array1);
-  print(array2);
+//   print(array1);
+//   print(array2);
 
-  late var value = getValue();
-  print('Variable sudah dibuat');
-  print(value);
-}
+//   late var value = getValue();
+//   print('Variable sudah dibuat');
+//   print(value);
+// }
 
-String getValue() {
-  print('getValue() dipanggil');
-  return 'Mukti Muhtadi';
+// String getValue() {
+//   print('getValue() dipanggil');
+//   return 'Mukti Muhtadi';
+// }
+
+//   int number1 = 10;
+//   double number2 = 10.5;
+
+//   print(number1);
+//   print(number2);
+
+//   num number3 = 10;
+//   print(number3);
+
+//   number3 = 10.5;
+//   print(number3);
+
+//   bool finish = false;
+//   print(finish);
+
+//   finish = true;
+//   print(finish);
+
+//   String firstName = 'Mukti';
+//   String lastName = "Muhtadi";
+
+//   var fullName = '$firstName ${lastName}';
+
+//   print(fullName);
+
+//   var text = 'this is \*dart\* \$cool';
+
+//   print(text);
+
+//   var name1 = firstName + " " + lastName;
+//   var name2 = 'Mukti' ' Muhtadi';
+
+//   print(name1);
+//   print(name2);
+
+//   var longString = '''
+// this is long string
+// miltiline string
+// learning dart
+// lorem ipsum
+// ''';
+
+//   print(longString);
+
+  dynamic variable = 100;
+  print(variable);
+
+  variable = true;
+  print(variable);
+
+  variable = 'Mukti';
+  print(variable);
+
+  // var inputString = '1000';
+  // var inputInt = int.parse(inputString);
+  // var inputDouble = double.parse(inputString);
+
+  // print(inputString);
+  // print(inputInt);
+  // print(inputDouble);
+
+  // var intToDouble = inputInt.toDouble();
+  // var doubleToInt = inputDouble.toInt();
+
+  // print(intToDouble);
+  // print(doubleToInt);
+
+  // var intToString = inputInt.toString();
+  // var doubleToString = inputDouble.toString();
+
+  // print(intToString);
+  // print(doubleToString);
+
+  // var doubleFromInt = inputInt.toDouble();
+  // var intFromDouble = inputDouble.toInt();
+
+  // print(doubleFromInt);
+  // print(intFromDouble);
+
+  // var stringFromInt = inputInt.toString();
+  // var stringFromDouble = inputDouble.toString();
+
+  // print(stringFromInt);
+  // print(stringFromDouble);
+
+  var inputString = 'true';
+  var inpurBool = inputString == 'true';
+
+  print(inputString);
+  print(inpurBool);
+
+  // var stringFromBool = inputBoolean.toString();
+
+  // print(stringFromBool);
+
+  // var boolToString = inputBoolean.toString();
+
+  // print(boolToString);
+
+  var result1 = 10 + 10;
+  var result2 = 100 - 10;
+  var result3 = -5;
+  var result4 = 5;
+  var result5 = 5 * 5;
+  var result6 = 10 / 3;
+  var result7 = 10 ~/ 3;
+  var result8 = 10 % 3;
+
+  print(result1);
+  print(result2);
+  print(result3);
+  print(result4);
+  print(result5);
+  print(result6);
+  print(result7);
+  print(result8);
+
+  var first = 10;
+  var second = 3;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second);
+  print(first ~/ second);
+  print(first % second);
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('mukti' == 'mukti');
+  print('mukti' != 'mukti');
 }
